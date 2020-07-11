ec977e8b3de11aec8aa949a39e9ca0f2420fc82a
Josue <jlopezc@pucp.edu.pe>
2020-07-11 12:01:34 -0500

    Firebase and facebook auth

---
+++ app/src/main/java/com/example/lab4_grupo2/FacebookActivity.java
@@ -0,0 +1,5 @@
+package com.example.lab4_grupo2;
+
+public class FacebookActivity {
+
+}
--- app/src/main/java/com/example/lab4_grupo2/MainActivity.java
+++ app/src/main/java/com/example/lab4_grupo2/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.lab4_grupo2;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
+        CallbackManager callbackManager = CallbackManager.Factory.create();
+
     }
+
+
 }
