976617f76b0875af9d70a673358ac34a27747e1d
Rodrigo5023 <a20163458@pucp.edu.pe>
2020-07-13 23:23:23 -0500

    Avance nuevo de listar y ver detalles 2

--- app/src/main/java/com/example/lab4_grupo2/DetallesActivity.java
+++ app/src/main/java/com/example/lab4_grupo2/DetallesActivity.java
@@ -1,6 +1,7 @@
 package com.example.lab4_grupo2;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,6 +15,7 @@ import com.example.lab4_grupo2.EntidadesRuiz.Comentario;
 import com.example.lab4_grupo2.EntidadesRuiz.Fotografia;
 import com.example.lab4_grupo2.EntidadesRuiz.dtoComentario;
 import com.example.lab4_grupo2.EntidadesRuiz.dtoFotografia;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -37,33 +39,57 @@ public class DetallesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalles);
-        final Fotografia fotografia = new Fotografia();
+        final Fotografia photography = new Fotografia();
         final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
         
         // Obtencio del parametro enviado: NombreFotografia
         final String nombreFotografia = getIntent().getStringExtra("nombreFotografia");
 
-        databaseReference.child("fotos").child(nombreFotografia).addValueEventListener(new ValueEventListener() {
+
+        databaseReference.child("fotos").child(nombreFotografia).addChildEventListener(new ChildEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
                 if (dataSnapshot.exists()){
-
+                    Fotografia fotografia = dataSnapshot.getValue(Fotografia.class);
                     String autor = dataSnapshot.child("nombre").getValue().toString(); fotografia.setUsuarioCreador(autor);
                     String fechaSubida = dataSnapshot.child("fechaSubida").getValue().toString(); fotografia.setFechaSubida(fechaSubida);
                     String descripcion = dataSnapshot.child("descripcion").getValue().toString(); fotografia.setDescripcion(descripcion);
                     String nombreFotografia = dataSnapshot.getKey();; fotografia.setDescripcion(descripcion); fotografia.setNombreFotografia(nombreFotografia);
                     // Falta agregar Fotografia !!!!!!!
-                } }
+                    fotografia = photography;
+                }
+            }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {  Toast.makeText(DetallesActivity.this,"No existe fotos en la base de datos.",Toast.LENGTH_LONG).show();
-            } });
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                if (dataSnapshot.exists()){
+                    Fotografia fotografia = dataSnapshot.getValue(Fotografia.class);
+                    String autor = dataSnapshot.child("nombre").getValue().toString(); fotografia.setUsuarioCreador(autor);
+                    String fechaSubida = dataSnapshot.child("fechaSubida").getValue().toString(); fotografia.setFechaSubida(fechaSubida);
+                    String descripcion = dataSnapshot.child("descripcion").getValue().toString(); fotografia.setDescripcion(descripcion);
+                    String nombreFotografia = dataSnapshot.getKey();; fotografia.setDescripcion(descripcion); fotografia.setNombreFotografia(nombreFotografia);
+                    // Falta agregar Fotografia !!!!!!
+                    fotografia = photography;
+                }
 
+            }
 
-        databaseReference.child("lista").child(nombreFotografia).child("comentarios").addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) { }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) { }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) { }
+        });
+
+
+
+        // COMENTARIO
+        databaseReference.child("fotos").child(nombreFotografia).child("comentarios").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
                 if (dataSnapshot.exists()){
                     Long longitudComentarios = (dataSnapshot.getChildrenCount());
                     int longitud2 = longitudComentarios.intValue();
@@ -79,13 +105,42 @@ public class DetallesActivity extends AppCompatActivity {
                             inicial++;
                         } //final del IF
                     } // final del FOR
-                    fotografia.setListaComentarios(listaComentarios);
+                    photography.setListaComentarios(listaComentarios);
                 } }
 
+
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Toast.makeText(DetallesActivity.this,"No existen comentarios en la base de datos.",Toast.LENGTH_LONG).show();
-            } });
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                if (dataSnapshot.exists()){
+                    Long longitudComentarios = (dataSnapshot.getChildrenCount());
+                    int longitud2 = longitudComentarios.intValue();
+                    listaComentarios = new Comentario[longitud2];
+
+                    int inicial = 0;
+                    for (DataSnapshot children:dataSnapshot.getChildren()){
+                        if (dataSnapshot.exists()) {
+                            Comentario comentario = dataSnapshot.getValue(Comentario.class);
+                            String fechaComentario = dataSnapshot.child("fecha").getValue().toString(); comentario.setFechaComentario(fechaComentario);
+                            String horaComentario = dataSnapshot.child("hora").getValue().toString(); comentario.setHoraComentario(horaComentario);
+                            listaComentarios[inicial] = comentario;
+                            inicial++;
+                        } //final del IF
+                    } // final del FOR
+                    photography.setListaComentarios(listaComentarios);
+                } }
+
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) { }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) { }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) { }
+        });
+
+
 
 
 
@@ -97,10 +152,10 @@ public class DetallesActivity extends AppCompatActivity {
         recyclerView.setAdapter(comentariosAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(DetallesActivity.this));
 
-        TextView autor = findViewById(R.id.textViewAutor); autor.setText(fotografia.getUsuarioCreador());
+        TextView autor = findViewById(R.id.textViewAutor); autor.setText(photography.getUsuarioCreador());
         // ImageView foto = findViewById(R.id.imageViewFotografia); foto.setText(fotografia.getUsuarioCreador()); // AGREGAR FOTOGRAFIA!!!!!
-        TextView descripcion = findViewById(R.id.textViewDescripcion); descripcion.setText(fotografia.getDescripcion());
-        TextView fecha = findViewById(R.id.textViewFecha); fecha.setText(fotografia.getFechaSubida());
+        TextView descripcion = findViewById(R.id.textViewDescripcion); descripcion.setText(photography.getDescripcion());
+        TextView fecha = findViewById(R.id.textViewFecha); fecha.setText(photography.getFechaSubida());
         TextView cantidadComentarios = findViewById(R.id.textViewComentarios); cantidadComentarios.setText(listaComentarios.length);
 
     }
